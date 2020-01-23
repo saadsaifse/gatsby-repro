28330790ad799301b36d22ed5eaede721b8e2d24
Saad Saif <msaadsaif5@gmail.com>
2020-01-23 22:26:22 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,37 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content`,
+      },
+    },
+    //`gatsby-transformer-remark`,
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 850,
+            },
+          },
+          "gatsby-remark-copy-linked-files",
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            options: {
+              offsetY: "100",
+              maintainCase: false,
+              removeAccents: true,
+            },
+          },
+          "gatsby-remark-prismjs",
+        ],
+      },
+    },
+  ],
 }
